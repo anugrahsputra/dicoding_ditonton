c93bff363fa01972b1d260a5991dd167e7ffcba6
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-12-01 20:55:07 +0700

    usecase tv

---
+++ lib/domain/usecases/tv/search_tv.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv/tv.dart';
+import 'package:ditonton/domain/repositories/tv_repository.dart';
+
+class SearchTv {
+  final TvRepository repository;
+
+  SearchTv(this.repository);
+
+  Future<Either<Failure, List<Tv>>> execute(String query) {
+    return repository.searchTv(query);
+  }
+}
