c012537cf58af2e8316761ea67dc46f4f336db7c
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-12-01 21:02:06 +0700

    change file directiory

--- test/domain/usecases/get_watchlist_status_test.dart
+++ test/domain/usecases/get_watchlist_status_test.dart
@@ -1,4 +1,4 @@
-import 'package:ditonton/domain/usecases/get_watchlist_status.dart';
+import 'package:ditonton/domain/usecases/movie/get_watchlist_status.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
--- test/domain/usecases/save_watchlist_test.dart
+++ test/domain/usecases/save_watchlist_test.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:ditonton/domain/usecases/save_watchlist.dart';
+import 'package:ditonton/domain/usecases/movie/save_watchlist.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
