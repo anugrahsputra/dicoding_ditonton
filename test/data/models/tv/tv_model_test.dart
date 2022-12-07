f7f74b67b8c3e43e07c50c35e7ba5c168aac8e6c
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-12-07 15:34:31 +0700

    add tv model test

---
+++ test/data/models/tv/tv_model_test.dart
@@ -0,0 +1,45 @@
+import 'package:ditonton/data/models/tv/tv_model.dart';
+import 'package:ditonton/domain/entities/tv/tv.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tTvModel = TvModel(
+    backdropPath: 'backdropPath',
+    firstAirDate: '2022-10-12',
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originCountry: ['originCountry'],
+    originalLanguage: 'originalLanguage',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tTv = Tv(
+    backdropPath: 'backdropPath',
+    firstAirDate: '2022-10-12',
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originCountry: ['originCountry'],
+    originalLanguage: 'originalLanguage',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  test(
+    "should be a subclass of Tv entity",
+    () async {
+      final result = tTvModel.toEntity();
+      expect(result, tTv);
+    },
+  );
+}
